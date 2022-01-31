859182b5fbb83686c2c259dac33c1e5c85be617f
Mak <mak1312001@gmail.com>
2022-01-31 16:19:33 +0530

    update310122

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
                     
                     <div class="col-lg-6 col-sm-5 col-xs-12 col-md-5">
                         <div class="slide__thumb">
-                            <img src="images/slider/fornt-img/1.png" alt="slider images">
+                            <img src="images/slider/fornt-img/sb.png" alt="slider images">
                         </div>
                     </div>
                 </div>
